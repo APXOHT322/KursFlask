10a62790e58e73c60fbdf7c3c630ac97774022e2
APXOHT322 <tara44314@gmail.com>
2026-03-28 14:14:37 +0300

    Add files via upload

--- app/manage_courses.py
+++ app/manage_courses.py
@@ -461,9 +461,14 @@ from selenium.webdriver.chrome.service import Service
 
 def get_driver():
     options = Options()
-    options.add_argument("--headless")  # браузер без окна
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--single-process")
 
     return webdriver.Chrome(
         service=Service(ChromeDriverManager().install()),
